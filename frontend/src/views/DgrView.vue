e43cac40e41462e8bb3c3ebb60faf1f3ef0047d9
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-05-23 13:18:23 +0500

    create DgrView

---
+++ frontend/src/views/DgrView.vue
@@ -0,0 +1,20 @@
+<template>
+    <dgr-component/>
+</template>
+
+<script>
+import DgrComponent from "@/components/DgrComponent.vue"
+
+    export default{
+        name:'dgr-app',
+        data(){
+            return {
+                app:{},
+            }
+        },
+
+        components:{
+        DgrComponent
+        }
+    }
+</script>
