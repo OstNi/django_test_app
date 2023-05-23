93e4be63bebdc7f91c936da3f3d879367cd3625e
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-05-23 13:18:38 +0500

    create TwblockView

---
+++ frontend/src/views/TwblockView.vue
@@ -0,0 +1,20 @@
+<template>
+    <twblock-component/>
+</template>
+
+<script>
+import TwblockComponent from "@/components/TwblockComponent.vue"
+
+    export default{
+        name:'twblock-app',
+        data(){
+            return {
+                app:{},
+            }
+        },
+
+        components:{
+            TwblockComponent
+        }
+    }
+</script>
